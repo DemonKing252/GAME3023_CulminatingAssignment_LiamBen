cd5693b03ae1542a13ca4f70e3e0d15042b815c7
DemonKing252 <46793138+DemonKing252@users.noreply.github.com>
2021-10-02 20:32:06 -0400

    Pause menu added

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,6 +13,9 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private Animator anim;
 
+    public Canvas pauseCanvas;
+    bool isPaused = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,13 +23,33 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();    
     }
 
+    public void OnSaveAndQuit()
+    {
+        // save functionality here . . .
+
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void OnExitWithoutSaving()
+    {
+
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        pauseCanvas.gameObject.SetActive(false);
+        isPaused = false;
+    }
+
+
+
     // Update is called once per frame
     void Update()
     {
         // Add to Diary: Didn't realize that delta time was already used in velocity 
 
-        var horiz = Input.GetAxisRaw("Horizontal"); 
-        var vert = Input.GetAxisRaw("Vertical"); 
+        var horiz = Input.GetAxisRaw("Horizontal");
+        var vert = Input.GetAxisRaw("Vertical");
 
         rb.velocity = new Vector2(horiz * speed, vert * speed);
 
@@ -38,8 +63,24 @@ public class PlayerController : MonoBehaviour
         {
             transform.localScale = new Vector3(-1f, 1f, 1f);
         }
-        
-        //Debug.Log("lateral: " + lateralSpeed.ToString());
-            
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = (isPaused ? false : true);
+
+            if (isPaused)
+            {
+                Time.timeScale = 0f;
+                pauseCanvas.gameObject.SetActive(true);
+            }
+            else
+            {
+                Time.timeScale = 1f;
+                pauseCanvas.gameObject.SetActive(false);
+
+            }
+
+            //Debug.Log("lateral: " + lateralSpeed.ToString());
+
+        }
     }
 }
