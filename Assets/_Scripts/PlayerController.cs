838c38ceb30ab2fef5cde867ac35d4dec702857c
DemonKing252 <46793138+DemonKing252@users.noreply.github.com>
2021-10-02 20:05:27 -0400

    Player animation and faces proper direction

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -9,10 +9,12 @@ public class PlayerController : MonoBehaviour
     
     
     private Rigidbody2D rb;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();    
     }
 
@@ -25,5 +27,19 @@ public class PlayerController : MonoBehaviour
         var vert = Input.GetAxisRaw("Vertical"); 
 
         rb.velocity = new Vector2(horiz * speed, vert * speed);
+
+        float lateralSpeed = new Vector2(rb.velocity.x / speed, rb.velocity.y / speed).magnitude;
+        anim.SetFloat("_Speed", lateralSpeed);
+        if (rb.velocity.x > 0f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else if (rb.velocity.x < 0f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        
+        //Debug.Log("lateral: " + lateralSpeed.ToString());
+            
     }
 }
