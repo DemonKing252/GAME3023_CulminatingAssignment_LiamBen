915e69f3e8ea4896c2586abb054e32cfbf5f877a
DemonKing252 <46793138+DemonKing252@users.noreply.github.com>
2021-10-07 12:08:16 -0400

    New background image for battle

--- Assets/_Scripts/PlayerUI.cs
+++ Assets/_Scripts/PlayerUI.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class PlayerUI : MonoBehaviour
 {
     public Canvas encounterCanvas;
+    public GameObject playerui; // disable overworld ui while the player is in this encounter.
     // Animation event
     public void ShowEncounter()
     {
         encounterCanvas.gameObject.SetActive(true);
+        playerui.SetActive(false);
     }
 }
